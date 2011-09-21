43d1a1feb236c29ee48c8ee7a6343b6689791817
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2011-09-21 16:31:14 +0200

    progress on research chapter

--- src/test/test-batman.c
+++ src/test/test-batman.c
@@ -89,8 +89,8 @@ __wrap_llc_rx(packet_t *packet)
 
   switch (cnt) {
     case 0:
-      printf("Receiving unidirectional OGM confirmation from 0x000b\n");
-      printf("Expected behavior: adding 0xb to routing table, no reply\n");
+      printf("Receiving unidirectional OGM confirmation from 0xb\n");
+      printf("Expected: adding 0xb to routing table, no reply\n");
 
 //      batman_one_second_elapsed();
 //      batman_thread();
@@ -99,43 +99,43 @@ __wrap_llc_rx(packet_t *packet)
       ogm->flags = (1 << OGM_FLAG_UNIDIRECTIONAL) | (1 << OGM_FLAG_IS_DIRECT);
       ogm->ttl = 49;
       ogm->seqno = 0;
-      ogm->originator_addr = 0x000a;
-      ogm->sender_addr = 0x000b;
+      ogm->originator_addr = 0xa;
+      ogm->sender_addr = 0xb;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 1:
-      printf("Receiving HELLO OGM from 0x000b\n");
-      printf("Expected behavior: replying with direct flag set (bidirectional confirmation)\n");
+      printf("Receiving HELLO OGM from 0xb\n");
+      printf("Expected: replying with direct flag set (bidirectional confirmation)\n");
 
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 50;
       ogm->seqno = 0;
-      ogm->originator_addr = 0x000b;
-      ogm->sender_addr = 0x000b;
+      ogm->originator_addr = 0xb;
+      ogm->sender_addr = 0xb;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 2:
-      printf("Receiving HELLO OGM from 0x000c\n");
-      printf("Expected behavior: replying with direct flag set and unidirectional flag set (unidirectional confirmation)\n");
+      printf("Receiving HELLO OGM from 0xc\n");
+      printf("Expected: replying with direct flag set and unidirectional flag set (unidirectional confirmation)\n");
 
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 50;
       ogm->seqno = 1;
-      ogm->originator_addr = 0x000c;
-      ogm->sender_addr = 0x000c;
+      ogm->originator_addr = 0xc;
+      ogm->sender_addr = 0xc;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 3:
-      printf("Receiving bidirectional OGM confirmation from 0x000c\n");
-      printf("Expected behavior: adding 0xc to routing table, no reply\n");
+      printf("Receiving bidirectional OGM confirmation from 0xc\n");
+      printf("Expected: adding 0xc to routing table, no reply\n");
 
 //      batman_one_second_elapsed();
 //      batman_thread();
@@ -144,128 +144,146 @@ __wrap_llc_rx(packet_t *packet)
       ogm->flags = (1 << OGM_FLAG_IS_DIRECT);
       ogm->ttl = 49;
       ogm->seqno = 1;
-      ogm->originator_addr = 0x000a;
-      ogm->sender_addr = 0x000c;
+      ogm->originator_addr = 0xa;
+      ogm->sender_addr = 0xc;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 4:
-      printf("Receiving OGM rebroadcast from 0x000d via 0x000c\n");
-      printf("Expected behavior: adding 0xd to routing table, no reply\n");
+      printf("Receiving OGM rebroadcast from 0xd via 0xc\n");
+      printf("Expected: adding 0xd to routing table, no reply\n");
 
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 49;
       ogm->seqno = 23;
-      ogm->originator_addr = 0x000d;
-      ogm->sender_addr = 0x000c;
+      ogm->originator_addr = 0xd;
+      ogm->sender_addr = 0xc;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 5:
-      printf("Receiving HELLO OGM from 0x000c\n");
-      printf("Expected behavior: replying to 0x000c with direct flag set, updating sequence number for 0xc in routing table\n");
+      printf("Receiving HELLO OGM from 0xc\n");
+      printf("Expected: replying to 0xc with direct flag set, updating sequence number for 0xc in routing table\n");
 
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 50;
       ogm->seqno = 1;
-      ogm->originator_addr = 0x000c;
-      ogm->sender_addr = 0x000c;
+      ogm->originator_addr = 0xc;
+      ogm->sender_addr = 0xc;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 6:
-      printf("Receiving HELLO OGM from 0x000b with maximum seqno value\n");
-      printf("Expected behavior: replying with direct flag set (bidirectional confirmation)\n");
+      printf("Receiving HELLO OGM from 0xb with maximum seqno value\n");
+      printf("Expected: replying with direct flag set (bidirectional confirmation)\n");
 
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 50;
       ogm->seqno = UINT16_MAX;
-      ogm->originator_addr = 0x000b;
-      ogm->sender_addr = 0x000b;
+      ogm->originator_addr = 0xb;
+      ogm->sender_addr = 0xb;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 7:
-      printf("Receiving HELLO OGM from 0x000b with seqno value overflowed\n");
-      printf("Expected behavior: ");
+      printf("Receiving HELLO OGM from 0xb with seqno value overflowed\n");
+      printf("Expected: sequence number for node 0xb reset to 0\n");
 
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 50;
       ogm->seqno = 0;
-      ogm->originator_addr = 0x000b;
-      ogm->sender_addr = 0x000b;
+      ogm->originator_addr = 0xb;
+      ogm->sender_addr = 0xb;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 8:
+      printf("Received OGM reply from 0xb with direct flag set\n");
+      printf("Expected: No action. Route to 0xb already exists.\n");
+
       ogm->version = BATMAN_VERSION;
       ogm->flags = (1 << OGM_FLAG_IS_DIRECT);
       ogm->ttl = 49;
       ogm->seqno = 2;
-      ogm->originator_addr = 0x000a;
-      ogm->sender_addr = 0x000b;
+      ogm->originator_addr = 0xa;
+      ogm->sender_addr = 0xb;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 9:
+      printf("Received unidirectional OGM confirmation from 0xd directly (not via 0xc).\n");
+      printf("Expected: Add new route entry with a direct connection to 0xd. Note that a route to 0xd already exists via 0xc\n");
+
       ogm->version = BATMAN_VERSION;
       ogm->flags = (1 << OGM_FLAG_UNIDIRECTIONAL) | (1 << OGM_FLAG_IS_DIRECT);
       ogm->ttl = 49;
       ogm->seqno = 2;
-      ogm->originator_addr = 0x000a;
-      ogm->sender_addr = 0x000d;
+      ogm->originator_addr = 0xa;
+      ogm->sender_addr = 0xd;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 10:
+      printf("Received OGM from 0xd.\n");
+      printf("Expected: Reply to 0xd with direct flag set. Update count in routing table\n");
+
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 50;
       ogm->seqno = 3;
-      ogm->originator_addr = 0x000d;
-      ogm->sender_addr = 0x000d;
+      ogm->originator_addr = 0xd;
+      ogm->sender_addr = 0xd;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 11:
+      printf("Receive rebroadcasted OGM from 0xd via 0xc.\n");
+      printf("Expected: Update count for 0xd via 0xc in routing table\n");
+
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 49;
       ogm->seqno = 24;
-      ogm->originator_addr = 0x000d;
-      ogm->sender_addr = 0x000c;
+      ogm->originator_addr = 0xd;
+      ogm->sender_addr = 0xc;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 12:
+      printf("Receive next rebroadcasted OGM from 0xd via 0xc.\n");
+      printf("Expected: Update count for 0xd via 0xc in routing table\n");
+
       ogm->version = BATMAN_VERSION;
       ogm->flags = 0;
       ogm->ttl = 49;
       ogm->seqno = 25;
-      ogm->originator_addr = 0x000d;
-      ogm->sender_addr = 0x000c;
+      ogm->originator_addr = 0xd;
+      ogm->sender_addr = 0xc;
 
       llc->type = BROADCAST;
       llc->len = OGM_HEADER_SIZE;
       break;
     case 13:
-      header->gateway_addr = 0x000a;
+      printf("Receive unicast message sent from unknown node 0x1 targeted to 0xd.\n");
+      printf("Expected: Target node 0xd is known. Highest rating is gateway 0xc. Therefore rebroadcast via 0xc.\n");
+
+      header->gateway_addr = 0xa;
       header->originator_addr = 0xffff;
-      header->target_addr = 0x000d;
-      header->sender_addr = 0x0001;
+      header->target_addr = 0xd;
+      header->sender_addr = 0x1;
       header->ttl = 50;
 
       memcpy(msg, TEST_STRING_1, strlen(TEST_STRING_1)+1);
@@ -274,10 +292,13 @@ __wrap_llc_rx(packet_t *packet)
       llc->len = BATMAN_HEADER_SIZE + strlen(msg) + 1;
       break;
     case 14:
-      header->gateway_addr = 0x000a;
-      header->originator_addr = 0x000d;
-      header->target_addr = 0x000c;
-      header->sender_addr = 0x0001;
+      printf("Received unicast message sent from unknown node 0x1 targeted to 0xc.\n");
+      printf("Expected: Target node 0xc is known, it is a direct neighbour. Therefore rebroadcast directly to 0xc.\n");
+
+      header->gateway_addr = 0xa;
+      header->originator_addr = 0xd;
+      header->target_addr = 0xc;
+      header->sender_addr = 0x1;
       header->ttl = 50;
 
       memcpy(msg, TEST_STRING_2, strlen(TEST_STRING_2)+1);
@@ -286,22 +307,13 @@ __wrap_llc_rx(packet_t *packet)
       llc->len = BATMAN_HEADER_SIZE + strlen(msg)+1;
       break;
     case 15:
-      header->gateway_addr = 0x000a;
-      header->originator_addr = 0x000d;
-      header->target_addr = 0x000c;
-      header->sender_addr = 0x0001;
-      header->ttl = 50;
+      printf("Received unicast message targeted to us.\n");
+      printf("Expected: \n");
 
-      memcpy(msg, TEST_STRING_2, strlen(TEST_STRING_2)+1);
-
-      llc->type = UNICAST;
-      llc->len = BATMAN_HEADER_SIZE + strlen(msg)+1;
-      break;
-    case 16:
-      header->gateway_addr = 0x000a;
-      header->originator_addr = 0x000d;
-      header->target_addr = 0x000a;
-      header->sender_addr = 0x0001;
+      header->gateway_addr = 0xa;
+      header->originator_addr = 0xd;
+      header->target_addr = 0xa;
+      header->sender_addr = 0x1;
       header->ttl = 0;
 
       memcpy(msg, TEST_STRING_3, strlen(TEST_STRING_3)+1);
@@ -309,11 +321,11 @@ __wrap_llc_rx(packet_t *packet)
       llc->type = UNICAST;
       llc->len = BATMAN_HEADER_SIZE + strlen(msg)+1;
       break;
-    case 17:
-      header->gateway_addr = 0x000c;
-      header->originator_addr = 0x000d;
-      header->target_addr = 0x000b;
-      header->sender_addr = 0x000d;
+    case 16:
+      header->gateway_addr = 0xc;
+      header->originator_addr = 0xd;
+      header->target_addr = 0xb;
+      header->sender_addr = 0xd;
       header->ttl = 50;
 
       memcpy(msg, TEST_STRING_3, strlen(TEST_STRING_3)+1);
@@ -321,13 +333,13 @@ __wrap_llc_rx(packet_t *packet)
       llc->type = UNICAST;
       llc->len = BATMAN_HEADER_SIZE + strlen(msg)+1;
       break;
-    case 18:
+    case 17:
     default:
       printf("end of tests\n");
       break;
   }
 
-#define CNT_TESTS 17
+#define CNT_TESTS 16
 
   if (cnt <= CNT_TESTS) {
     printf("\nrx:\n");
@@ -358,7 +370,7 @@ main(int argc, char **argv)
   clock_init();
   config_init();
   sei();
-  config_set(0, 0x000a);
+  config_set(0, 0xa);
   batman_init();
 
   while (testing) {
